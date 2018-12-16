704840c8cbee0e2b82f0fa4f9fe2a8ebd9824c73
Imre von Geczy <imre.geczy@yahoo.co.uk>
2018-12-16 17:48:15 +0000

    step-3 add Fibonacci sequence with phpspec

--- spec/Fibonacci/CalculationSpec.php
+++ spec/Fibonacci/CalculationSpec.php
@@ -8,8 +8,28 @@ use Prophecy\Argument;
 
 class CalculationSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_returns_zero_in_array_when_is_on_level_zero()
     {
-        $this->shouldHaveType(Calculation::class);
+        $this->sequence(0)->shouldReturn([0]);
+    }
+
+    function it_returns_zero_and_one_in_array_when_is_on_level_one()
+    {
+        $this->sequence(1)->shouldReturn([0, 1]);
+    }
+
+    function it_returns_zero_and_one_one_in_array_when_is_on_level_two()
+    {
+        $this->sequence(2)->shouldReturn([0, 1, 1]);
+    }
+
+    function it_returns_zero_and_one_one_two_in_array_when_is_on_level_three()
+    {
+        $this->sequence(3)->shouldReturn([0, 1, 1, 2]);
+    }
+
+    function it_returns_zero_and_one_one_two_three_in_array_when_is_on_level_four()
+    {
+        $this->sequence(4)->shouldReturn([0, 1, 1, 2, 3]);
     }
 }
--- src/Fibonacci/Calculation.php
+++ src/Fibonacci/Calculation.php
@@ -4,4 +4,24 @@ namespace Fibonacci;
 
 class Calculation
 {
+    /**
+     * @param int $level
+     * @return array
+     */
+    public function sequence(int $iteration): array
+    {
+        $result = [0];
+
+        for ($number = 0; $number < $iteration; $number++) {
+            $last = count($result);
+
+            $numberLast = $result[$last - 1] > 0 ? $result[$last - 1] : 1;
+            $numberBeforeLast = isset($result[$last - 2]) ? $result[$last - 2] : 0;
+
+            $next = $numberBeforeLast + $numberLast;
+            array_push($result, $next);
+        }
+
+        return $result;
+    }
 }
